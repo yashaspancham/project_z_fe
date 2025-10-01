213c0a9ec1f35cfe83356a52cc190f2078d81f99
Yashas <yashaspancham@gmail.com>
2025-10-01 18:50:25 +0530

    Yashas-feat-newFavIcon

--- app/page.tsx
+++ app/page.tsx
@@ -7,7 +7,7 @@ export default function Home() {
   return (
     <main className="w-full h-[100vh] flex items-center justify-center">
       <div className="flex flex-col items-center gap-5">
-        <p className="text-4xl">Welcome to Reflections</p>
+        <p className="text-4xl">Welcome to Flow</p>
         <button 
         onClick={()=>router.push("/sign-in")}
         className="bg-blue-500 p-2 w-40 rounded-lg text-md hover:bg-blue-600 hover:cursor-pointer text-white">
