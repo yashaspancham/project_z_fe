2054a03cf7a54d96f703d6ad1768b7c7faf24047
Yashas <yashaspancham@gmail.com>
2025-10-09 00:30:08 +0530

    Yashas-feat-BetterLoading

--- app/entries/page.tsx
+++ app/entries/page.tsx
@@ -1,9 +1,14 @@
 import EntriesPageComp from "@/components/entriesPageComp";
+import SuspenseLoading from "@/components/SuspenseLoading";
 import { Suspense } from "react";
 export default function Home() {
   return (
-  <Suspense fallback={<div>Loading...</div>}>
-    <EntriesPageComp />
-  </Suspense>
-  )
+    <Suspense
+      fallback={
+        <SuspenseLoading />
+      }
+    >
+      <EntriesPageComp />
+    </Suspense>
+  );
 }
